ead6af9ecd1fddb441191fdcbf3f40013b3e0b78
hindisimarwin <sherwinsevilla7@gmail.com>
2024-12-22 22:06:30 +0800

    Delete LA directory

--- LA/la28.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-name = "JUSTIN SOMINE"
-windows = tk.Tk()
-windows.title(f"OOP LA28 {name}")
-windows.geometry("1366x768")
-windows.configure(bg="Green")
-
-windows.mainloop()
--- LA/la6.py
+++
@@ -1,8 +0,0 @@
-class Laptop:
-    def __init__(self, brand, model):
-        pass
-    def laptop_info(self):
-        pass
-
-asus = Laptop ("Asus", "2023")
-print(asus.laptop_info())    
